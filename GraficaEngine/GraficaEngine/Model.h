4162c5c4c1abb8a322fb8aa75d75ab0652a3afde
Emiliano <emidesejas@gmail.com>
2021-04-11 20:11:59 -0300

    Add Model class.

---
+++ GraficaEngine/GraficaEngine/Model.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <iostream>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+#include "Shader.h"
+#include "Mesh.h"
+#include "DebugLog.h"
+#include "Texture.h"
+
+class Model
+{
+public:
+    Model(char* path)
+    {
+        loadModel(path);
+    }
+    void draw(Shader& shader);
+private:
+    std::vector<Mesh> _meshes;
+    std::string _directory;
+
+    void loadModel(std::string path);
+    void processNode(aiNode* node, const aiScene* scene);
+    Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+    std::vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type,
+        std::string typeName);
+};
